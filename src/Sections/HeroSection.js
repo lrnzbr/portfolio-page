d3e4ac74f327d7997b5d8f3fbcf948a52c48bec1
Lorenzo Brown <lrnzbr@gmail.com>
2021-12-21 18:27:37 -0600

    add Resume

--- src/Sections/HeroSection.js
+++ src/Sections/HeroSection.js
@@ -4,6 +4,7 @@ import Header1 from "../StyleComponents/Header1";
 import Header2 from "../StyleComponents/Header2";
 import Button from "../StyleComponents/Button";
 import "./sections.css"
+import resume from "../assets/BrownLorenzoResume.pdf"
 
 export default function HeroSection() {
     return(
@@ -14,7 +15,7 @@ export default function HeroSection() {
        <li><Button text="A Bit about Me" action="#About"/> </li>
        <li><Button text="Projects" action="#Projects"/> </li>
        <li><Button text="Teaching" action="#Teaching"/> </li>
-       <li><Button text="Download my Resumé" action="#"/> </li>
+       <li><Button text="Download my Resumé" action={resume}/> </li>
        <li><Button text="See my LinkedIn" action="https://www.linkedin.com/in/lorenzobrown"/> </li>
        <li><Button text="Check Out my GitHub" action="https://www.github.com/lrnzbr"/> </li>
        <li><Button text="Shoot me a Whatsapp" action="https://wa.me/525524161721"/> </li>
